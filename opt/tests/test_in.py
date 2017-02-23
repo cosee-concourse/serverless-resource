d06558f934d0a634458463e7dd71dc981a67501d
Jannis Schaefer <j.schaefer@estwx.de>
2017-02-23 12:10:39 +0100

    Added UnitTest for In

--- opt/tests/test_in.py
+++ opt/tests/test_in.py
@@ -1,5 +1,6 @@
 import unittest
-from unittest.mock import MagicMock
+from io import TextIOWrapper
+from unittest.mock import MagicMock, patch
 
 import in_
 from concourse import test_common
@@ -24,3 +25,24 @@ class TestInput(unittest.TestCase):
             """)
 
         self.assertEqual(in_.execute('/'), -1)
+
+    def test_json(self):
+        with patch("builtins.open", create=True) as mock_open:
+            mock_open.return_value = MagicMock(spec=TextIOWrapper)
+
+            test_common.put_stdin(
+                """
+                {
+                  "source": {
+                    "apiKey": "apiKey123",
+                    "secretKey": "secretKey321"
+                  },
+                  "version": {
+                    "stage": "1.0.0-rc.42"
+                  }
+                }
+                """)
+
+            self.assertEqual(in_.execute('/'), 0)
+            file_handle = mock_open.return_value.__enter__.return_value
+            file_handle.write.assert_called_with('1.0.0-rc.42')
