6d3aad0713f9d12cb26a5ddecb953c22ad7c9770
Jannis Schaefer <j.schaefer@estwx.de>
2017-03-01 12:40:57 +0100

    Removed --pull Tag

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ docker build --pull \
         -t $CONTAINER_IMAGE_NAME:latest \
         --build-arg serverlessVersion=1.8.0 .
 
-docker build --pull \
+docker build \
         -t $CONTAINER_IMAGE_NAME:1.7.0 \
         -t $CONTAINER_IMAGE_NAME:1.7 \
         --build-arg serverlessVersion=1.7.0 .
