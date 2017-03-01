b21f404decf43b24cd52f42cdeb82e78f760d905
Jannis Schaefer <j.schaefer@estwx.de>
2017-03-01 12:36:09 +0100

    Build for Serverless 1.8

--- build.sh
+++ build.sh
@@ -1,7 +1,12 @@
 docker build --pull \
+        -t $CONTAINER_IMAGE_NAME:1.8.0 \
+        -t $CONTAINER_IMAGE_NAME:1.8 \
+        -t $CONTAINER_IMAGE_NAME:latest \
+        --build-arg serverlessVersion=1.8.0 .
+
+docker build --pull \
         -t $CONTAINER_IMAGE_NAME:1.7.0 \
         -t $CONTAINER_IMAGE_NAME:1.7 \
-        -t $CONTAINER_IMAGE_NAME:latest \
         --build-arg serverlessVersion=1.7.0 .
 
 docker build \
