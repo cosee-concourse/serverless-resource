cd952ab6cc350b7b73b6189fb8edfbbe995962c1
Jannis Schaefer <jannis.schaefer@cosee.biz>
2018-06-15 14:46:59 +0200

    update serverless to 1.27.3

--- build.sh
+++ build.sh
@@ -1,63 +1,68 @@
 docker build --pull \
         -t $CONTAINER_IMAGE_NAME:latest \
+        -t $CONTAINER_IMAGE_NAME:1.27.3 \
+        -t $CONTAINER_IMAGE_NAME:1.27 \
+        --build-arg serverlessVersion=1.27.3 .
+
+docker build \
         -t $CONTAINER_IMAGE_NAME:1.25.0 \
         -t $CONTAINER_IMAGE_NAME:1.25 \
         --build-arg serverlessVersion=1.25.0 .
 
-docker build --pull \
+docker build \
         -t $CONTAINER_IMAGE_NAME:1.24.1 \
         -t $CONTAINER_IMAGE_NAME:1.24 \
         --build-arg serverlessVersion=1.24.1 .
 
-docker build --pull \
+docker build \
         -t $CONTAINER_IMAGE_NAME:1.24.0 \
         --build-arg serverlessVersion=1.24.0 .
 
-docker build --pull \
+docker build \
         -t $CONTAINER_IMAGE_NAME:1.23.0 \
         -t $CONTAINER_IMAGE_NAME:1.23 \
         --build-arg serverlessVersion=1.23.0 .
 
-docker build --pull \
+docker build \
         -t $CONTAINER_IMAGE_NAME:1.22.0 \
         -t $CONTAINER_IMAGE_NAME:1.22 \
         --build-arg serverlessVersion=1.22.0 .
 
-docker build --pull \
+docker build \
         -t $CONTAINER_IMAGE_NAME:1.21.1 \
         -t $CONTAINER_IMAGE_NAME:1.21 \
         --build-arg serverlessVersion=1.21.1 .
 
-docker build --pull \
+docker build \
         -t $CONTAINER_IMAGE_NAME:1.21.0 \
         --build-arg serverlessVersion=1.21.0 .
 
-docker build --pull \
+docker build \
         -t $CONTAINER_IMAGE_NAME:1.13.2 \
         -t $CONTAINER_IMAGE_NAME:1.13 \
         --build-arg serverlessVersion=1.13.2 .
 
-docker build --pull \
+docker build \
         -t $CONTAINER_IMAGE_NAME:1.12.1 \
         -t $CONTAINER_IMAGE_NAME:1.12 \
         --build-arg serverlessVersion=1.12.1 .
 
-docker build --pull \
+docker build \
         -t $CONTAINER_IMAGE_NAME:1.11.0 \
         -t $CONTAINER_IMAGE_NAME:1.11 \
         --build-arg serverlessVersion=1.11.0 .
 
-docker build --pull \
+docker build \
         -t $CONTAINER_IMAGE_NAME:1.10.2 \
         -t $CONTAINER_IMAGE_NAME:1.10 \
         --build-arg serverlessVersion=1.10.2 .
 
-docker build --pull \
+docker build \
         -t $CONTAINER_IMAGE_NAME:1.9.0 \
         -t $CONTAINER_IMAGE_NAME:1.9 \
         --build-arg serverlessVersion=1.9.0 .
 
-docker build --pull \
+docker build \
         -t $CONTAINER_IMAGE_NAME:1.8.0 \
         -t $CONTAINER_IMAGE_NAME:1.8 \
         --build-arg serverlessVersion=1.8.0 .
